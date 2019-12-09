9456279e4dcaa5f1141349d0f478c3e2c8116153
PanTajemnic <idziejczak3@gmail.com>
2019-12-09 17:10:43 +0100

    drobne poprawki

--- commands/kasza.js
+++ commands/kasza.js
@@ -1,5 +1,5 @@
 exports.run = (client, message, args) => {
-	if (msg.author.id=="340604514820161536")
-		msg.channel.send("<@493080797943955476> ty cieniasie");
-	else msg.reply(" jesteś cieniasem")
+	if (message.author.id=="340604514820161536")
+		message.channel.send("<@493080797943955476> ty cieniasie");
+	else message.reply(" jesteś cieniasem")
 }
--- events/message.js
+++ events/message.js
@@ -8,5 +8,18 @@ module.exports = (client, message) => {
   const cmd = client.commands.get(command);
   if (!cmd) return;
 
+//Taki kod z stackoverflowa jak się chce odpowiedzieć od razu
+/*if (command === 'spec'){
+        message.author.send("See or Change?");
+        const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
+        console.log(collector)
+        collector.on('collect', message => {
+            if (message.content === "See") {
+                message.channel.send("You Want To See Someones Spec OK!");
+            } else if (message.content === "Change") {
+                message.channel.send("You Want To Change Your Spec OK!");
+            }
+        })*/
+
   cmd.run(client, message, args);
 };
