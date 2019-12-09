a43ab45f321770b834f2aea3aa1652bedd60757e
PanTajemnic <idziejczak3@gmail.com>
2019-12-09 15:34:22 +0100

    powitanie nowych członków

--- commands/ping.js
+++ commands/ping.js
@@ -1,3 +1,4 @@
 exports.run = (client, message, args) => {
     message.reply('pong');
+    //client.emit("guildMemberAdd", message.member);
 }
---
+++ events/guildMemberAdd.js
@@ -0,0 +1,4 @@
+module.exports = (client, member) => {
+  const defaultChannel = client.channels.get("469291361531723797");
+  defaultChannel.send(`Siemano ${member.user} na serwerze przyjacieli z ZSK.`).catch(console.error);
+}
