4fdd9056a27070e5994c51f8255064c137d0e83c
Alset-N <34602864+ukaszogi@users.noreply.github.com>
2019-12-12 23:04:24 +0100

    Staryyy, jeszcze tylko stary

--- commands/$zsk.js
+++ commands/$zsk.js
@@ -3,45 +3,101 @@ exports.run = (client, message, args) => {
   
   if(args[0]=="spr") {
     if(args[1]=="up") {
-	switch (args[2]) {
-	    case "1":
-		const dane = message.content.replace("$zsk spr up 1 ",'');
-		message.channel.send(dane);
-		break;
-	    case "2":
-		
-		break;
-	}
+        switch (args[2]) {
+            case "1":
+            warto = [args[3],args[4],message.content.replace("$zsk spr up 1 "+args[3]+" "+args[4]+" ",'')];
+            message.channel.send(warto[0]);
+            message.channel.send(warto[1]);
+            message.channel.send(warto[2]);
+            funk.sprNowy(1,warto)
+            break;
+            case "2":
+            warto = [args[3],args[4],message.content.replace("$zsk spr up 2 "+args[3]+" "+args[4]+" ",'')];
+            message.channel.send(warto[0]);
+            message.channel.send(warto[1]);
+            message.channel.send(warto[2]);
+            funk.sprNowy(2,warto)
+            break;
+            default:
+            message.channel.send("Błędna grupa");
+        }
     }
     else if(args[1]=="1") {
-	if (funk.sprBlisko(1,1).length==0) message.channel.send("Brak sprawdzianów")
-	funk.sprSend(1,parseInt(args[2]),message);
+        if (funk.sprBlisko(1,1).length==0) message.channel.send("Brak sprawdzianów")
+        else if (args[2]==0||args.length==2) funk.sprSend(1,1000000,message);
+        else funk.sprSend(1,parseInt(args[2]),message);
     }
     else if(args[1]=="2") {
-	if (funk.sprBlisko(2,1).length==0) message.channel.send("Brak sprawdzianów")
-    	funk.sprSend(2,parseInt(args[2]),message);
+        if (funk.sprBlisko(2,1).length==0) message.channel.send("Brak sprawdzianów")
+        else if (args[2]==0||args.length==2) funk.sprSend(2,1000000,message);
+        else funk.sprSend(2,parseInt(args[2]),message);
     }
+    else message.channel.send("Błędna grupa");
   }
   else if(args[0]=="krt") {
-    if(args[1]=="up") {}
+    if(args[1]=="up") {
+        switch (args[2]) {
+            case "1":
+            warto = [args[3],args[4],message.content.replace("$zsk krt up 1 "+args[3]+" "+args[4]+" ",'')];
+            message.channel.send(warto[0]);
+            message.channel.send(warto[1]);
+            message.channel.send(warto[2]);
+            funk.krtNowy(1,warto)
+            break;
+            case "2":
+            warto = [args[3],args[4],message.content.replace("$zsk krt up 2 "+args[3]+" "+args[4]+" ",'')];
+            message.channel.send(warto[0]);
+            message.channel.send(warto[1]);
+            message.channel.send(warto[2]);
+            funk.krtNowy(2,warto)
+            break;
+            default:
+            message.channel.send("Błędna grupa");
+        }
+    }
     else if(args[1]=="1") {
-	if (funk.krtBlisko(1,1).length==0) message.channel.send("Brak kartkówek")
-	funk.krtSend(1,parseInt(args[2]),message);
+        if (funk.krtBlisko(1,1).length==0) message.channel.send("Brak kartkówek")
+        else if (args[2]==0||args.length==2) funk.krtSend(1,1000000,message);
+        funk.krtSend(1,parseInt(args[2]),message);
     }
     else if(args[1]=="2") {
-	if (funk.krtBlisko(2,1).length==0) message.channel.send("Brak kartkówek")
-	funk.krtSend(2,parseInt(args[2]),message);
+        if (funk.krtBlisko(2,1).length==0) message.channel.send("Brak kartkówek")
+        else if (args[2]==0||args.length==2) funk.krtSend(2,1000000,message);
+        funk.krtSend(2,parseInt(args[2]),message);
     }
+    else message.channel.send("Błędna grupa");
   }
   else if(args[0]=="zad") {
-    if(args[1]=="up") {}
+    if(args[1]=="up") {
+        switch (args[2]) {
+            case "1":
+            warto = [args[3],args[4],message.content.replace("$zsk zad up 1 "+args[3]+" "+args[4]+" ",'')];
+            message.channel.send(warto[0]);
+            message.channel.send(warto[1]);
+            message.channel.send(warto[2]);
+            funk.zadNowy(1,warto)
+            break;
+            case "2":
+            warto = [args[3],args[4],message.content.replace("$zsk zad up 2 "+args[3]+" "+args[4]+" ",'')];
+            message.channel.send(warto[0]);
+            message.channel.send(warto[1]);
+            message.channel.send(warto[2]);
+            funk.zadNowy(2,warto)
+            break;
+            default:
+            message.channel.send("Błędna grupa");
+        }
+    }
     else if(args[1]=="1") {
-	if (funk.zadBlisko(1,1).length==0) message.channel.send("Brak zadań domowych")
-	funk.zadSend(1,parseInt(args[2]),message);
+        if (funk.zadBlisko(1,1).length==0) message.channel.send("Brak zadań domowych")
+        else if (args[2]==0||args.length==2) funk.zadSend(1,1000000,message);
+        funk.zadSend(1,parseInt(args[2]),message);
     }
     else if(args[1]=="2") {
-	if (funk.zadBlisko(1,1).length==0) message.channel.send("Brak zadań domowych")
-	funk.zadSend(2,parseInt(args[2]),message);
+        if (funk.zadBlisko(1,1).length==0) message.channel.send("Brak zadań domowych")
+        else if (args[2]==0||args.length==2) funk.zadSend(2,1000000,message);
+        funk.zadSend(2,parseInt(args[2]),message);
     }
+    else message.channel.send("Błędna grupa");
   }
 }
