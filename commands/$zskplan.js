50af7eeaea6f1e6a6463afaef03ee52ef17db246
PanTajemnic <idziejczak3@gmail.com>
2020-05-12 22:38:45 +0200

    Dodanie do komendy planu "dzisiaj", "jutro" lub wybrana przez użytkownika data np. "2020 4 30"

--- commands/$zskplan.js
+++ commands/$zskplan.js
@@ -25,10 +25,94 @@ exports.run = (client, message, args) => {
     const idOddzial = jdo[pozycja].idOddzial
     const idOkresKlasyfikacyjny = jdo[pozycja].idOkresKlasyfikacyjny
     const idUczen = jdo[pozycja].idUczen
+    const d = new Date()
+    let dataPoczatkowa = null, dataKoncowa = null, dataWybrana = null
+    let rokWybrany = args[0], miesiacWybrany = args[1], dzienWybrany = args[2], dzienTygWybrany = null, dzienNazwa = null
+    let rokPocz = null, miesiacPocz = null, dzienPocz = null
+    let rokKon = null, miesiacKon = 0, dzienKon = null
+
+    console.log(`args[0] = ${args[0]}, args[1] = ${args[1]}, args[2] = ${args[2]}, `)
+
+    let days = ["Niedziela", "Poniedziałek", "Wtorek", "środa", "Czwartek", "Piątek", "Sobota"];
+    if (args[0]=="dzisiaj") {
+        rokWybrany = d.getFullYear()
+        miesiacWybrany = d.getMonth()+1
+        dzienWybrany = d.getDate()
+        dzienTygWybrany = d.getDay()
+        dzienNazwa = days[d.getDay()]
+        mdd = null
+        if (miesiacWybrany<10) mdd = "0"+miesiacWybrany
+        dataWybrana = rokWybrany+"-"+mdd+"-"+dzienWybrany
+        console.log(dataWybrana)
+    } else if (args[0]=="jutro") {
+        rokWybrany = d.getFullYear()
+        miesiacWybrany = d.getMonth()+1
+        dzienWybrany = d.getDate()+1
+        dzienTygWybrany = d.getDay()+1
+        dzienNazwa = days[dzienTygWybrany]
+        mdd = null
+        if (miesiacWybrany<10) mdd = "0"+miesiacWybrany
+        dataWybrana = rokWybrany+"-"+mdd+"-"+dzienWybrany
+        console.log(dataWybrana)
+    } else {
+        rokWybrany = args[0]
+        miesiacWybrany = args[1]
+        dzienWybrany = args[2]
+        dtw = new Date().setFullYear(rokWybrany, miesiacWybrany-1, dzienWybrany)
+        dzienTygWybrany = new Date(dtw).getDay()
+        let days = ["Niedziela", "Poniedziałek", "Wtorek", "środa", "Czwartek", "Piątek", "Sobota"];
+        dzienNazwa = days[dzienTygWybrany]
+        mdd = null
+        if (miesiacWybrany<10) mdd = "0"+miesiacWybrany
+        dataWybrana = rokWybrany+"-"+mdd+"-"+dzienWybrany
+    }
+
+    if (dzienWybrany - (dzienTygWybrany - 1) < 0 && miesiacWybrany-1>0) {
+        rokPocz = rokWybrany
+        miesiacPocz = miesiacWybrany-1
+        dzienPocz = new Date(rokWybrany, miesiacWybrany, 0).getDate() + (dzienWybrany - (dzienTygWybrany - 1))
+        console.log("1 if pocz")
+    } else if (dzienWybrany - (dzienTygWybrany - 1) < 0 && miesiacWybrany-1<=0) {
+        rokPocz = rokWybrany-1
+        miesiacPocz = 12
+        dzienPocz = 31 + (dzienWybrany - (dzienTygWybrany - 1))
+        console.log("2 if pocz")
+    } else {
+        rokPocz = rokWybrany
+        miesiacPocz = miesiacWybrany
+        dzienPocz = dzienWybrany - (dzienTygWybrany - 1)
+        console.log("3 if pocz"+dzienWybrany+dzienTygWybrany)
+    }
+    if (miesiacPocz<10) miesiacPocz = "0"+miesiacPocz
+    if (dzienPocz<10) dzienPocz = "0"+dzienPocz
+    dataPoczatkowa = rokPocz + "-" + miesiacPocz + "-" + dzienPocz
+    console.log(`\nData początkowa: ` + dataPoczatkowa)
+
+    if (dzienWybrany + (7 - dzienTygWybrany) > new Date(rokWybrany, miesiacWybrany, 0).getDate() && miesiacWybrany < 12) {
+        rokKon = rokWybrany
+        miesiacKon = 1 + parseInt(miesiacWybrany)
+        // if (miesiacKon<10) miesiacKon = "0"+miesiacKon
+        dzienKon = (7 - dzienTygWybrany) - (new Date(rokWybrany, miesiacWybrany, 0).getDate() - dzienWybrany)
+        console.log("1 if kon")
+    } else if (dzienWybrany + (7 - dzienTygWybrany) > new Date(rokWybrany, miesiacWybrany, 0).getDate() && miesiacWybrany >= 12) {
+        rokKon = rokWybrany+1
+        miesiacKon = 1
+        dzienKon = (7 - dzienTygWybrany) - (new Date(rokWybrany, miesiacWybrany, 0).getDate() - dzienWybrany)
+        console.log("2 if kon")
+    } else {
+        rokKon = rokWybrany
+        miesiacKon = miesiacWybrany
+        dzienKon = dzienWybrany + (7 - dzienTygWybrany)
+        console.log("3 if kon")
+    }
+    if (miesiacKon<10) miesiacKon = "0"+miesiacKon
+    if (dzienKon<10) dzienKon = "0"+dzienKon
+    dataKoncowa = rokKon + "-" + miesiacKon + "-" + dzienKon
+    console.log(`\nData koncowa: ` + dataKoncowa)
 
     let formPlan = {
-        "DataPoczatkowa": "2020-05-11",
-        "DataKoncowa": "2020-05-17",
+        "DataPoczatkowa": dataPoczatkowa,
+        "DataKoncowa": dataKoncowa,
         "IdOddzial": idOddzial,
         "IdOkresKlasyfikacyjny": idOkresKlasyfikacyjny,
         "IdUczen": idUczen,
@@ -61,7 +145,7 @@ exports.run = (client, message, args) => {
             var lekcja = "", co = "", gdzie = ""
 
             lekcje.forEach(function (item) {
-                    if (item.DzienTekst === "2020-05-11" && item.PlanUcznia === true) {
+                    if (item.DzienTekst == dataWybrana && item.PlanUcznia === true) {
                         tabwynik.push(item.NumerLekcji + "," + item.Sala + "," + item.PrzedmiotNazwa)
                     }
                 }
@@ -74,7 +158,7 @@ exports.run = (client, message, args) => {
                 co += cale[2] + "\n"
             })
             let Embed = new Discord.RichEmbed()
-                .setTitle("**PLAN LEKCJI**")
+                .setTitle(`**PLAN LEKCJI - ${dzienNazwa}**`)
                 .setColor("#58ff1d")
                 .addField("Nr", lekcja, true)
                 .addField("Sala", gdzie, true)
