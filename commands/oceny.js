29f7a3d5b82bc9022248ccb7a487581cae8a2f9b
PanTajemnic <idziejczak3@gmail.com>
2020-06-02 18:35:34 +0200

    Add command for grades

--- commands/oceny.js
+++ commands/oceny.js
@@ -1,4 +1,4 @@
-exports.run = (client, message/*, args*/) => {
+exports.run = (client, message, args) => {
 
     const jdo = require("../wrazliweDane.json")
     let pozycja = null
@@ -10,6 +10,17 @@ exports.run = (client, message/*, args*/) => {
         message.channel.send("najpierw musisz się zalogować pisząc bezpośrednio **do mnie** prywatną wiadomość używając komendy: `$zsklogin [token] [symbol] [pin]`")
         return
     }
+    let przedmiotWybrany = "";
+    if (args.length>0) {
+        args.forEach(function (item) {
+            przedmiotWybrany += item + " "
+        })
+        przedmiotWybrany = przedmiotWybrany.slice(0,-1)
+    }
+    else  {
+        przedmiotWybrany = "wszystkie"
+    }
+    przedmiotWybrany = przedmiotWybrany.toLowerCase()
 
     const uuidv4 = require('uuidv4'),
         signer = require("@wulkanowy/uonet-request-signer"),
@@ -19,7 +30,7 @@ exports.run = (client, message/*, args*/) => {
         certificateKey1 = jdo[pozycja].certyfikatKlucz,
         password1 = require("../auth.json").password,
         certificate1 = jdo[pozycja].certyfikatPfx,
-        urlSprawdziany = 'https://lekcjaplus.vulcan.net.pl/poznan/000088/mobile-api/Uczen.v3.Uczen/Sprawdziany',
+        urlSprawdziany = 'https://lekcjaplus.vulcan.net.pl/poznan/000088/mobile-api/Uczen.v3.Uczen/Oceny',
         urlSlowniki = 'https://lekcjaplus.vulcan.net.pl/poznan/000088/mobile-api/Uczen.v3.Uczen/Slowniki',
         timekey = Math.floor(Date.now() / 1000),
         timekey1 = timekey - 1,
@@ -39,7 +50,7 @@ exports.run = (client, message/*, args*/) => {
     console.log(`Data początkowa: ${dataPoczatkowa}`)
     console.log(`Data koncowa: ${dataKoncowa}`)
 
-    let formSprawdziany = {
+    let formOceny = {
         "DataPoczatkowa": dataPoczatkowa,
         "DataKoncowa": dataKoncowa,
         "IdOddzial": idOddzial,
@@ -58,7 +69,7 @@ exports.run = (client, message/*, args*/) => {
         "RemoteMobileAppVersion": "19.4.1.436",
         "RemoteMobileAppName": "VULCAN-Android-ModulUcznia"
     }
-    let formData = JSON.stringify(formSprawdziany)
+    let formData = JSON.stringify(formOceny)
     let formData2 = JSON.stringify(formSlowniki)
 
     signer.signContent(password1, certificate1, formData).then(signed => {
@@ -76,13 +87,13 @@ exports.run = (client, message/*, args*/) => {
         }, function (err, res, body) {
             const json = JSON.parse(body);
             console.log("Status: " + json.Status);
-            const sprKartJson = json.Data
+            const zadaniaJson = json.Data
             let nauczyciele, przedmioty
-            const sprawdzianyIKartkowki = []
-            let jestSprawdzianem, opis, dataTekst, przedmiot, nauczyciel
+            const zadaniaLista = []
+            let opis, ocena, waga, dataTekst, przedmiot, nauczyciel
 
-            sprKartJson.forEach(function (item) {
-                    sprawdzianyIKartkowki.push(item["DataTekst"] + ",,," + item["Rodzaj"] + ",,," + item["Opis"] + ",,," + item["IdPrzedmiot"] + ",,," + item["IdPracownik"])
+            zadaniaJson.forEach(function (item) {
+                    zadaniaLista.push(item["DataUtworzeniaTekst"] + ",,," + item["Wpis"] + ",,," + item["Waga"] + ",,," + item["Opis"] + ",,," + item["IdPrzedmiot"] + ",,," + item["IdPracownikD"])
                 }
             )
 
@@ -102,59 +113,49 @@ exports.run = (client, message/*, args*/) => {
                     nauczyciele = data["Nauczyciele"]
                     przedmioty = data["Przedmioty"]
 
-                    if (sprawdzianyIKartkowki.length > 0) {
-                        sprawdzianyIKartkowki.sort()
-                        let liData = 0, liOpis = 0, liPrzedmiot = 0
-                        sprawdzianyIKartkowki.forEach(function (item) {
+                    if (zadaniaLista.length > 0) {
+                        zadaniaLista.sort()
+                        zadaniaLista.reverse()
+                        let liData = 0, liOcena = 0, liWaga = 0, liOpis = 0, liPrzedmiot = 0
+                        zadaniaLista.forEach(function (item) {
                             let cale = item.split(",,,")
-                            if (cale[0].length > liData) liData = cale[0].length
-                            if (cale[2].length > liOpis) liOpis = cale[2].length
+                            let bl = false
                             przedmioty.forEach(function (item) {
-                                if (item["Id"].toString() === cale[3]) {
+                                if (item["Id"].toString() === cale[4] && (item["Nazwa"].toString().toLowerCase() === przedmiotWybrany || przedmiotWybrany === "wszystkie")) {
                                     let nazwa = item["Nazwa"]
                                     if (nazwa.length > liPrzedmiot) liPrzedmiot = nazwa.length
+                                    //TODO: Naprawić bardzo nieoptymalne rozwiązanie
+                                    if (cale[0].length > liData) liData = cale[0].length
+                                    if (cale[1].length > liOcena) liOcena = cale[1].length
+                                    if (cale[2].length > liWaga) liWaga = cale[2].length
+                                    if (cale[3].length > liOpis) liOpis = cale[3].length
                                 }
                             })
                         })
-                        let calusienkie = `\nSPRAWDZIANY\ndata${spacja(liData - 1)}opis${spacja(liOpis - 1)}przedmiot${spacja(liPrzedmiot - 6)}nauczyciel\n`
-                        sprawdzianyIKartkowki.forEach(function (item) {
+                        let calusienkie = `\nOCENY\ndata${spacja(liData - 1)}wpis${spacja(liOcena - 1)}waga${spacja(liWaga - 1)}opis${spacja(liOpis - 1)}przedmiot${spacja(liPrzedmiot - 6)}nauczyciel\n`
+                        zadaniaLista.forEach(function (item) {
                             let cale = item.split(",,,")
-                            jestSprawdzianem = cale[1]
                             dataTekst = cale[0]
-                            opis = cale[2]
+                            ocena = cale[1]
+                            waga = cale[2]
+                            opis = cale[3]
                             if (opis.length === 0) opis = "(Brak opisu)"
                             przedmioty.forEach(function (item) {
-                                if (item["Id"].toString() === cale[3]) przedmiot = item["Nazwa"]
+                                if (item["Id"].toString() === cale[4]) przedmiot = item["Nazwa"]
                             })
                             nauczyciele.forEach(function (item) {
-                                if (item["Id"].toString() === cale[4]) nauczyciel = item["Imie"] + " " + item["Nazwisko"]
+                                if (item["Id"].toString() === cale[5]) nauczyciel = item["Imie"] + " " + item["Nazwisko"]
                             })
-                            if (jestSprawdzianem === "true") {
-                                calusienkie += dataTekst + spacja(3 + liData - dataTekst.length) + opis + spacja(3 + liOpis - opis.length) + przedmiot + spacja(3 + liPrzedmiot - przedmiot.length) + nauczyciel + "\n"
-                            }
-                        })
-                        calusienkie += `\nKARTKÓWKI\ndata${spacja(liData - 1)}opis${spacja(liOpis - 1)}przedmiot${spacja(liPrzedmiot - 6)}nauczyciel\n`
-                        sprawdzianyIKartkowki.forEach(function (item) {
-                            let cale = item.split(",,,")
-                            jestSprawdzianem = cale[1]
-                            dataTekst = cale[0]
-                            opis = cale[2]
-                            if (opis.length === 0) opis = "(Brak opisu)"
-                            przedmioty.forEach(function (item) {
-                                if (item["Id"].toString() === cale[3]) przedmiot = item["Nazwa"]
-                            })
-                            nauczyciele.forEach(function (item) {
-                                if (item["Id"].toString() === cale[4]) {
-                                    nauczyciel = item["Imie"] + " " + item["Nazwisko"]
-                                }
-                            })
-                            if (jestSprawdzianem === "false") {
-                                calusienkie += dataTekst + spacja(3 + liData - dataTekst.length) + opis + spacja(3 + liOpis - opis.length) + przedmiot + spacja(3 + liPrzedmiot - przedmiot.length) + nauczyciel + "\n"
+                            if (przedmiotWybrany === "wszystkie" || przedmiot.toString().toLowerCase() === przedmiotWybrany) {
+                                let calTest = calusienkie
+                                calTest += dataTekst + spacja(3 + liData - dataTekst.length) + ocena + spacja(3 + liOcena - ocena.length) + waga + spacja(3 + liWaga - waga.length) + opis + spacja(3 + liOpis - opis.length) + przedmiot + spacja(3 + liPrzedmiot - przedmiot.length) + nauczyciel + "\n"
+                                if (calTest.length<2000)
+                                    calusienkie += dataTekst + spacja(3 + liData - dataTekst.length) + ocena + spacja(3 + liOcena - ocena.length) + waga + spacja(3 + liWaga - waga.length) + opis + spacja(3 + liOpis - opis.length) + przedmiot + spacja(3 + liPrzedmiot - przedmiot.length) + nauczyciel + "\n"
                             }
                         })
                         message.channel.send("```" + calusienkie + "```")
                     } else {
-                        message.channel.send("Nie znalazłem żadnych sprawdzianów na następny miesiąc.")
+                        message.channel.send("Nie znalazłem żadnych zadań domowych na następny miesiąc.")
                     }
                 });
             });
