09aa05c124f06f38582856829fa5a4ca482aa451
PanTajemnic <idziejczak3@gmail.com>
2020-06-07 12:59:39 +0200

    fix error in los command

--- commands/los.js
+++ commands/los.js
@@ -6,6 +6,6 @@ module.exports = {
     execute(client, message, args) {
         if (args[0] != null && Number.isInteger(Math.floor(Math.random() * args[0])))
             message.channel.send(Math.floor(Math.random() * args[0]));
-        else msg.channel.send("wpisz liczbe np. los 100");
+        else message.channel.send("wpisz liczbe np. los 100");
     }
 }
