52c9c1e50583a002fefa8aed5349626c4837e1e0
Luncenok <idziejczak3@gmail.com>
2020-12-09 09:18:51 +0100

    Use yarn

---
+++ commands/america.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: "america",
+    description: "Odpowiada: FUCK YEAH!!!",
+    aliases: ['ameryka'],
+    usage: 'america',
+    category: 'fun',
+    execute(client, message) {
+        message.channel.send("FUCK YEAH!!!");
+    }
+}
