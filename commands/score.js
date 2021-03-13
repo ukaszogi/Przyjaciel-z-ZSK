a5d27ec96297a92075aa8e741a81700770adebb5
Kasza <34602864+ukaszogi@users.noreply.github.com>
2021-03-14 00:42:07 +0100

    KOlejny krok
    nie ogarniam bazę danych

--- commands/score.js
+++ commands/score.js
@@ -4,7 +4,31 @@ module.exports = {
     aliases: [],
     usage: 'score [twój wynik] | score',
     category: 'fun',
-    execute(client, message, args) {
-      message.channel.send(message.author.id, args[1]);
+    execute(client, message) {
+        const Keyv = require('keyv')
+        const keyv = new Keyv(client.config.pathToDatabase);
+
+        const args = message.content.split(/ +/);
+
+        function sortByValueAndIndex(data) {
+        
+        }
+
+        if (args[1]) {
+            keyv.delete(message.author.id)
+            keyv.set(message.author.id, args[1])
+            let prevScore = keyv.get(message.author.id)
+            if (prevScore) {
+                message.channel.send("Jest")
+            } else {
+                message.channel.send("Nie ma")
+            }
+            message.channel.send(`${prevScore}`)
+            message.channel.send(`<@${message.author.id}> pobił rekord z `);
+
+        } else {
+
+        }
+
     }
 }
